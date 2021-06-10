2a9349cb479e687b8c7d8c5a90b3aa034c28bd9f
Gini <net.gini@gmail.com>
2021-06-10 17:17:26 +0800

    update

--- terraform-gcp-demo/gcp-instance.tf
+++ terraform-gcp-demo/gcp-instance.tf
@@ -1,15 +1,14 @@
 provider "google" {
-  credentials = file("/home/devops/light-footing-289612-a4cac2800151.json")
-  project = "light-footing-289612"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  credentials = file("~/.gcp/mg-devops-febabc6f0c0a.json")
+  project = "mg-devops"
+  region  = "asia-southeast1" # Singapore
+  zone    = "asia-southeast1-a"
 }
 
-resource "google_compute_instance" "default" {
-  name         = "test"
+resource "google_compute_instance" "gcp_sandbox" {
+  name         = "gcp-sandbox"
   machine_type = "n1-standard-1"
   zone    = "us-central1-c"
-
   tags = ["foo", "bar"]
 
   boot_disk {
@@ -25,7 +24,6 @@ resource "google_compute_instance" "default" {
 
   network_interface {
     network = "default"
-
     access_config {
       // Ephemeral IP
     }
